dbd932f1f8ef9730baca4bdb9a474a3ae7becd24
Peacequare4 <ing.arassede.paix@gmail.com>
2025-10-03 03:26:45 +0100

    Fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/design-creative-portfolio/',
+  base: "/",
   plugins: [react( ), tailwindcss()],
   resolve: {
     alias: {
